d47e8b35810813f461323c77b73df4b665581008
SantiagoBedoyaU <santiago.bedoya35419@ucaldas.edu.co>
2024-10-27 09:16:31 -0500

    refactor file manager

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,25 @@
 use bcrypt::{hash, verify, DEFAULT_COST};
 use serde::Serialize;
 use std::{
+    env,
     fs::{self, write, File},
     io::{BufRead, BufReader, Write},
-    path::{self, Path},
+    path::Path,
 };
 
 #[derive(Serialize)]
 struct User {
     username: String,
     role: String,
+}
+
+#[derive(Serialize)]
+struct FileNode {
+    key: String,
+    title: String,
+    #[serde(rename = "isLeaf")]
+    is_leaf: bool,
+    children: Option<Vec<FileNode>>,
 }
 
 fn main() {
@@ -24,11 +34,21 @@ fn main() {
             sync_users_with_home,
             get_folder_files,
             write_user_file,
-            create_folder_filename,
-            delete_fodler_file,
+            create_file,
+            create_folder,
+            delete_folder_file,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn get_data_folder() -> Result<String, String> {
+    let current_dir = env::current_dir().map_err(|err| err.to_string())?;
+    let parent_dir = current_dir.parent().ok_or("No parent directory found")?;
+    let public_dir = parent_dir.join("public");
+
+    let home_dir = public_dir.join("home");
+    return Ok(home_dir.to_string_lossy().to_string());
 }
 
 #[tauri::command]
@@ -120,11 +140,27 @@ fn sync_users_with_home() -> Result<String, String> {
         let parts = line.split(':').collect::<Vec<&str>>();
 
         let folder_name = parts[parts.len() - 1];
-        let user_dir = format!("home/{}", folder_name);
+        let user_dir = format!("{}/{}", get_data_folder()?, folder_name);
 
-        match fs::create_dir_all(user_dir) {
+        match fs::create_dir_all(&user_dir) {
             Ok(_) => println!("Folder created {}", folder_name),
             Err(err) => eprintln!("Failed to create folder {}: {}", folder_name, err),
+        }
+
+        let subfolders = [
+            "Documents",
+            "Desktop",
+            "Downloads",
+            "Videos",
+            "Music",
+            "Photos",
+        ];
+        for subfolder in subfolders.iter() {
+            let subfolder_path = format!("{}/{}", user_dir, subfolder);
+            match fs::create_dir_all(&subfolder_path) {
+                Ok(_) => println!("Subfolder created {}", subfolder_path),
+                Err(err) => eprintln!("Failed to create subfolder {}: {}", subfolder, err),
+            }
         }
     }
 
@@ -132,28 +168,49 @@ fn sync_users_with_home() -> Result<String, String> {
 }
 
 #[tauri::command]
-fn get_folder_files(folder_name: &str) -> Result<Vec<(String, u64, String)>, String> {
-    let path = format!("home/{}", folder_name);
-    let dir = match fs::read_dir(path) {
-        Ok(dir) => dir,
-        Err(_) => return Err("Failed to read directory".to_string()),
-    };
+fn get_folder_files(folder_name: &str) -> Result<Vec<FileNode>, String> {
+    let user_path = format!("{}/{}", get_data_folder()?, folder_name);
+    let root_path = Path::new(&user_path);
+
+    if !root_path.is_dir() {
+        return Err("El directorio no existe o no es accesible".to_string());
+    }
+
+    fn build_tree(path: &Path) -> Result<FileNode, String> {
+        let metadata = path.metadata().map_err(|_| "Error al obtener metadata")?;
+        let is_leaf = !metadata.is_dir();
+        let title = path.file_name().unwrap().to_string_lossy().to_string();
+        let key = path.to_string_lossy().to_string();
+
+        let children = if !is_leaf {
+            let entries = fs::read_dir(path).map_err(|_| "Error al leer directorio")?;
+            let mut child_nodes = Vec::new();
+
+            for entry in entries {
+                let entry = entry.map_err(|_| "Error al obtener entrada de directorio")?;
+                let child_path = entry.path();
+                let child_node = build_tree(&child_path)?;
+                child_nodes.push(child_node);
+            }
+            Some(child_nodes)
+        } else {
+            None
+        };
+
+        Ok(FileNode {
+            key,
+            title,
+            is_leaf,
+            children,
+        })
+    }
 
     let mut files = Vec::new();
-    for entry in dir {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(_) => continue,
-        };
-        let file_name = entry.file_name().to_str().unwrap().to_string();
-        let file_type = if entry.file_type().unwrap().is_dir() {
-            "folder".to_string()
-        } else {
-            "file".to_string()
-        };
-        let file_size = entry.metadata().unwrap().len();
-
-        files.push((file_name, file_size, file_type));
+    for entry in fs::read_dir(root_path).map_err(|_| "Error al leer el directorio raíz")? {
+        let entry = entry.map_err(|_| "Error al obtener entrada de directorio raíz")?;
+        let path = entry.path();
+        let file_node = build_tree(&path)?;
+        files.push(file_node);
     }
 
     Ok(files)
@@ -161,9 +218,9 @@ fn get_folder_files(folder_name: &str) -> Result<Vec<(String, u64, String)>, Str
 
 #[tauri::command]
 fn write_user_file(folder_name: &str, filename: &str, content: &str) -> Result<String, String> {
-    let path = format!("home/{}/{}", folder_name, filename);
+    let user_path = format!("{}/{}/{}", get_data_folder()?, folder_name, filename);
 
-    if let Err(err) = write(path, content) {
+    if let Err(err) = write(user_path, content) {
         return Err(err.to_string());
     }
 
@@ -171,45 +228,41 @@ fn write_user_file(folder_name: &str, filename: &str, content: &str) -> Result<S
 }
 
 #[tauri::command]
-fn create_folder_filename(folder_name: &str, filename: &str) -> Result<String, String> {
-    let path = format!("home/{}/", folder_name);
-    let full_path = if filename.starts_with('/') {
-        let folder_name = filename.trim_start_matches('/');
-        format!("{}/{}", path, folder_name)
-    } else {
-        format!("{}/{}", path, filename)
-    };
-
-    if filename.starts_with('/') {
-        if let Err(err) = fs::create_dir_all(full_path) {
-            return Err(err.to_string());
-        }
-        Ok("Folder {} created successfully!".to_string())
-    } else {
-        let file_path = full_path.clone();
-        let parent_dir = file_path.clone();
-        let parent_dir = parent_dir.trim_end_matches(filename).to_string();
-        if let Err(err) = fs::create_dir_all(parent_dir) {
-            return Err(err.to_string());
-        }
-        File::create(file_path)
-            .map_err(|err| err.to_string())
-            .map(|_| "File created successfully!".to_string())
+fn create_file(foldername: &str, filepath: &str) -> Result<String, String> {
+    if !filepath.contains(foldername) {
+        return Err("Invalid route to file".to_string());
     }
+
+    File::create(filepath)
+        .map_err(|err| err.to_string())
+        .map(|_| "File created successfully!".to_string())
 }
 
 #[tauri::command]
-fn delete_fodler_file(folder_name: &str, filename: &str) -> Result<String, String> {
-    let path = format!("home/{}/{}", folder_name, filename);
-    if fs::metadata(&path).is_err() {
+fn create_folder(foldername: &str, folderpath: &str) -> Result<String, String> {
+    if !folderpath.contains(foldername) {
+        return Err("Invalid route to file".to_string());
+    }
+
+    fs::create_dir_all(folderpath)
+        .map_err(|err| err.to_string())
+        .map(|_| "Folder created successfully".to_string())
+}
+
+#[tauri::command]
+fn delete_folder_file(foldername: &str, filepath: &str) -> Result<String, String> {
+    if !filepath.contains(foldername) {
+        return Err("Invalid route".to_string());
+    }
+    if fs::metadata(&filepath).is_err() {
         return Err("File not found".to_string());
     }
-    if fs::metadata(&path).unwrap().is_dir() {
-        if let Err(err) = fs::remove_dir(&path) {
+    if fs::metadata(&filepath).unwrap().is_dir() {
+        if let Err(err) = fs::remove_dir(&filepath) {
             return Err(err.to_string());
         }
     } else {
-        if let Err(err) = fs::remove_file(&path) {
+        if let Err(err) = fs::remove_file(&filepath) {
             return Err(err.to_string());
         }
     }
--- src/app/desktop/components/FileManager.tsx
+++ src/app/desktop/components/FileManager.tsx
@@ -1,181 +1,103 @@
-import { invoke } from '@tauri-apps/api';
-import { Button, Dropdown, Form, Input, Modal } from 'antd';
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-
-interface UserFile {
-  id: number;
-  name: string;
-  type: 'file' | 'folder';
-  size: number;
-  thumbnail: string; // Add a thumbnail property for files
-}
-
-const FileManagerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-`;
-
-const FileListContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-`;
-
-const FileInfoContainer = styled.div`
-  width: 300px;
-  padding: 20px;
-  border-left: 1px solid #ddd;
-`;
-
-const FileItem = styled.div`
-  width: 180px;
-  height: 180px;
-  margin: 10px;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
-
-const FileThumbnail = styled.img`
-  width: 100px;
-  height: 100px;
-  border-radius: 10px;
-  margin-bottom: 10px;
-  margin-top: 10px;
-`;
-
-const FileName = styled.span`
-  font-size: 16px;
-  font-weight: bold;
-  color: white;
-  margin-bottom: 5px;
-`;
-
-const FileSize = styled.span`
-  font-size: 14px;
-  color: #666;
-`;
-
-const FileInfoTitle = styled.h2`
-  color: white;
-  margin-top: 0;
-`;
-
-const FileInfoDetail = styled.p`
-  font-size: 14px;
-  color: white;
-`;
+import { invoke } from "@tauri-apps/api";
+import { Tree, TreeDataNode, Form, Input, Button } from "antd";
+import { DirectoryTreeProps } from "antd/es/tree";
+import React, { useState, useEffect } from "react";
 
 const FileManager: React.FC = () => {
-  const [files, setFiles] = useState<UserFile[]>([]);
-  const [selectedFile, setSelectedFile] = useState<UserFile | null>(null);
-  const [isOpen, setIsOpen] = useState(false)
-  const [fileName, setFileName] = useState("")
+  const [files, setFiles] = useState<TreeDataNode[]>([]);
+  const [selectedFile, setSelectedFile] = useState<string>("");
+  const [selectedFilePath, setSelectedFilePath] = useState<string>("");
 
   const getFolderFiles = () => {
-    invoke<any>('get_folder_files', {
-      folderName: localStorage.getItem('folderName')!
-    }).then(result => {
-      const files: UserFile[] = [];
-      for (let i = 0; i < result.length; i++) {
-        const [fileName, size, type] = result[i];
-        files.push({
-          id: i + 1,
-          name: fileName,
-          type: type,
-          size: size,
-          thumbnail: type == 'file' ? '/images/file_logo.png' : '/images/folder_logo.png'
-        })
-      }
-      setFiles(files)
-    })
-  }
-
-  useEffect(() => {
-    getFolderFiles()
-  }, []);
-
-  const handleFileClick = (file: UserFile) => {
-    setSelectedFile(file);
+    setFiles([]);
+    invoke<TreeDataNode[]>("get_folder_files", {
+      folderName: localStorage.getItem("folderName")!,
+    }).then((result) => {
+      setFiles(result);
+    });
   };
 
-  const onDeleteFile = () => {
-    invoke<any>('delete_fodler_file', {
-      filename: selectedFile!.name,
-      folderName: localStorage.getItem('folderName')!
+  useEffect(() => {
+    getFolderFiles();
+  }, []);
+
+  const onCreateFile = () => {
+    invoke<String>('create_file', {
+      foldername: localStorage.getItem("folderName"),
+      filepath: `${selectedFilePath}/${selectedFile}`,
     }).then(() => {
       getFolderFiles()
-      setSelectedFile(null)
     }).catch(err => console.error(err))
-  }
+  };
 
-  const handleOkCreateFile = () => {
-    invoke('create_folder_filename', {
-      folderName: localStorage.getItem('folderName')!,
-      filename: fileName
+  const onCreateFolder = () => {
+    invoke<String>('create_folder', {
+      foldername: localStorage.getItem("folderName"),
+      folderpath: `${selectedFilePath}/${selectedFile}`,
     }).then(() => {
-      setIsOpen(false);
-      getFolderFiles();
+      getFolderFiles()
     }).catch(err => console.error(err))
   }
 
-  const items = [
-    {
-      label: <div onClick={() => setIsOpen(true)}>New File/Folder</div>,
-      key: 1
-    },
-  ]
+  const onDelete = () => {
+    invoke<String>('delete_folder_file', {
+      foldername: localStorage.getItem("folderName"),
+      filepath: selectedFilePath,
+    }).then(() => {
+      getFolderFiles()
+    }).catch(err => console.error(err));
+  }
+
+  const onSelect: DirectoryTreeProps["onSelect"] = (keys, info) => {
+    setSelectedFilePath(info.node.key as string);
+    setSelectedFile(info.node.title as string);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSelectedFile(value);
+  };
 
   return (
-    <FileManagerContainer>
-      <Modal open={isOpen} onOk={handleOkCreateFile} onCancel={() => setIsOpen(false)}>
-        <div style={{ margin: 20 }}>
-          <Form>
-            <Form.Item label="Name" rules={[{ required: true }]} extra="If you want to create a folder add / to the beginning of the name">
-              <Input onChange={e => setFileName(e.target.value)} />
-            </Form.Item>
-          </Form>
-        </div>
-      </Modal>
-      <Dropdown menu={{ items }} trigger={['contextMenu']}>
-        <FileListContainer>
-          {files.length === 0 ? (
-            <p>No files available.</p>
-          ) : (
-            files.map((file) => (
-              <FileItem key={file.id} onClick={() => handleFileClick(file)}>
-                <FileThumbnail src={file.thumbnail} />
-                <FileName>{file.name}</FileName>
-                <FileSize>{file.size} bytes</FileSize>
-              </FileItem>
-            ))
-          )}
-        </FileListContainer>
-      </Dropdown>
-      <FileInfoContainer>
-        {selectedFile && (
-          <div>
-            <FileInfoTitle>{selectedFile.name}</FileInfoTitle>
-            <FileInfoDetail>Type: {selectedFile.type}</FileInfoDetail>
-            <FileInfoDetail>Size: {selectedFile.size} bytes</FileInfoDetail>
-            <Button style={{ marginTop: 10 }} color="danger" onClick={onDeleteFile}>
-              Delete
-            </Button>
-          </div>
-        )}
-      </FileInfoContainer>
-    </FileManagerContainer>
+    <div>
+      <div>
+        <Input
+          value={selectedFile}
+          onChange={onChange}
+          style={{
+            marginBottom: 8,
+            marginTop: 8,
+            display: "inline",
+            width: "81.5%",
+            marginRight: 5,
+          }}
+        />
+        <Button
+          style={{ display: "inline", marginRight: 5 }}
+          onClick={onCreateFile}
+        >
+          Create File
+        </Button>
+        <Button
+          style={{ display: "inline", marginRight: 5 }}
+          onClick={onCreateFolder}
+        >
+          Create Folder
+        </Button>
+        <Button
+          style={{ display: "inline" }}
+          onClick={onDelete}
+        >
+          Delete
+        </Button>
+      </div>
+      <Tree.DirectoryTree
+        multiple
+        defaultExpandAll
+        treeData={files}
+        onSelect={onSelect}
+      />
+    </div>
   );
 };
 
